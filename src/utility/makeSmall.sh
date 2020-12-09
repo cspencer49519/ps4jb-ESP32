170bc251c62a984261fff9a0ad9141da494ccd62
Craig Spencer <graffix.420@gmail.com>
2020-12-09 13:57:17 -0500

    Change makeSmall.sh to compress all files (Thanks leeful)

--- src/utility/makeSmall.sh
+++ src/utility/makeSmall.sh
@@ -5,6 +5,20 @@ for file in `ls *.js`; do
     gzip -kf "$file" && \
     echo "Converting $file to byte array"
     xxd -i -u $file".gz" && \
-    echo "Deleting source file"
+    echo "Deleting source JS file"
+    rm -f "$file"
+done
+
+for file in `ls *.html`; do
+    echo "Compressing: $file"
+    gzip -kf "$file" && \
+    echo "Deleting source HTML file"
+    rm -f "$file"
+done
+
+for file in `ls *.bin`; do
+    echo "Compressing: $file"
+    gzip -kf "$file" && \
+    echo "Deleting source BIN file"
     rm -f "$file"
 done
